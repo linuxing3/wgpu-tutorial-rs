b4b2c483d3de40ce9092a6212b53d52a9375b4a6
linuxing3 <linuxing3@qq.com>
2023-05-12 23:32:33 +0800

    Commited by vagrant from /home/vagrant/workspace/rust/wgpu-tutorial-rs on 2023-05-12-23:32

--- src/layer.rs
+++ src/layer.rs
@@ -8,15 +8,13 @@ use crate::State;
 use crate::Camera;
 
 pub struct Layer {
-    pub renderer: LayerRenderer,
-    pub last_frame: Instant,
+    pub renderer : LayerRenderer,
+    pub last_frame : Instant,
 }
 
 impl Layer {
-    pub fn new(
-        context: &mut texture::Context,
-        bytes: &[u8],
-    ) -> Layer {
+    pub fn new(context : &mut texture::Context, bytes : &[u8]) -> Layer {
+
         let renderer = LayerRenderer::new(context, bytes);
 
         let last_frame = Instant::now();
@@ -27,29 +25,18 @@ impl Layer {
         }
     }
 
-    pub fn update(
-        &mut self,
-        width: f32,
-        height: f32,
-    ) {
-        self.renderer.update(width, height);
-    }
+    pub fn render(&mut self, context : &mut texture::Context, ui : &imgui::Ui) {
 
-    pub fn render(
-        &mut self,
-        context: &mut texture::Context,
-        ui: &imgui::Ui,
-    ) {
         let now = Instant::now();
 
         self.last_frame = now;
 
         self.renderer.set_data(context);
 
+        self.renderer.resize(context, ui);
+
         self.renderer.render(ui);
     }
 
-    pub fn renderer(&self) -> &LayerRenderer {
-        &self.renderer
-    }
+    pub fn renderer(&self) -> &LayerRenderer { &self.renderer }
 }
--- src/layer_renderer.rs
+++ src/layer_renderer.rs
@@ -2,35 +2,38 @@ use crate::texture::*;
 use image::*;
 
 pub struct LayerRenderer {
-    pub texture_id: imgui::TextureId,
-    pub data: RgbaImage,
-    pub height: u32,
-    pub width: u32,
+    pub texture_id : imgui::TextureId,
+    pub data : RgbaImage,
+    pub height : u32,
+    pub width : u32,
 }
 
 impl LayerRenderer {
-    pub fn new(
-        context: &mut Context,
-        bytes: &[u8],
-    ) -> LayerRenderer {
+    pub fn new(context : &mut Context, bytes : &[u8]) -> LayerRenderer {
+
         if bytes.len() == 0 {
+
             let size = wgpu::Extent3d {
-                width: 800,
-                height: 600,
+                width : 800,
+                height : 600,
                 ..Default::default()
             };
+
             let image = DynamicImage::new_rgba8(size.width, size.height);
+
             let texture = Texture::imgui_texture_from_raw(context, &image, size);
 
             // BUG:
             let texture_id = context.renderer.textures.insert(texture);
+
             return LayerRenderer {
                 texture_id,
-                data: image.to_rgba8(),
-                height: size.height,
-                width: size.width,
+                data : image.to_rgba8(),
+                height : size.height,
+                width : size.width,
             };
         } else {
+
             let (image, size) = Texture::imgui_image_from_raw(bytes);
 
             let texture = Texture::imgui_texture_from_raw(context, &image, size);
@@ -40,24 +43,17 @@ impl LayerRenderer {
 
             return LayerRenderer {
                 texture_id,
-                data: image.to_rgba8(),
-                height: size.height,
-                width: size.width,
+                data : image.to_rgba8(),
+                height : size.height,
+                width : size.width,
             };
         }
     }
 
-    pub fn render(
-        &mut self,
-        ui: &imgui::Ui,
-    ) {
-        self.push_to_command_list(ui);
-    }
+    pub fn render(&mut self, ui : &imgui::Ui) { self.push_to_command_list(ui); }
 
-    pub fn push_to_command_list(
-        &mut self,
-        ui: &imgui::Ui,
-    ) {
+    pub fn push_to_command_list(&mut self, ui : &imgui::Ui) {
+
         ui.invisible_button("Smooth Button", [self.height as f32, self.height as f32]);
 
         let draw_list = ui.get_window_draw_list();
@@ -78,14 +74,14 @@ impl LayerRenderer {
             .build();
     }
 
-    pub fn set_data(
-        &mut self,
-        context: &mut Context,
-    ) {
+    pub fn set_data(&mut self, context : &mut Context) {
+
         let (width, height) = (self.width, self.height);
 
         for y in height / 4..height * 3 / 4 {
+
             for x in width / 4..width * 3 / 4 {
+
                 let color = Self::per_pixel(x as u32, y as u32);
 
                 self.data.put_pixel(x, y, color);
@@ -102,8 +98,8 @@ impl LayerRenderer {
 
         let texture_config = imgui_wgpu::TextureConfig {
             size,
-            label: Some("raw texture"),
-            format: Some(wgpu::TextureFormat::Rgba8Unorm),
+            label : Some("raw texture"),
+            format : Some(wgpu::TextureFormat::Rgba8Unorm),
             ..Default::default()
         };
 
@@ -114,20 +110,42 @@ impl LayerRenderer {
         self.texture_id = context.renderer.textures.insert(texture);
     }
 
-    pub fn update(
-        &mut self,
-        width: f32,
-        height: f32,
-    ) {
+    pub fn resize(&mut self, context : &mut Context, ui : &imgui::Ui) {
 
-        // if self.data.width() != width | self.data.height() != height {
-        //
-        //
-        //
-        // }
+        let mut imgui_region_size = [self.width as f32, self.height as f32];
+
+        let new_imgui_region_size = Some(ui.content_region_avail());
+
+        if let Some(_size) = new_imgui_region_size {
+
+            // Resize render target, which is optional
+            if _size != imgui_region_size && _size[0] >= 1.0 && _size[1] >= 1.0 {
+
+                imgui_region_size = _size;
+
+                let scale = &ui.io().display_framebuffer_scale;
+
+                let texture_config = imgui_wgpu::TextureConfig {
+                    size : wgpu::Extent3d {
+                        width : (imgui_region_size[0] * scale[0]) as u32,
+                        height : (imgui_region_size[1] * scale[1]) as u32,
+                        ..Default::default()
+                    },
+                    usage : wgpu::TextureUsages::RENDER_ATTACHMENT
+                        | wgpu::TextureUsages::TEXTURE_BINDING,
+                    ..Default::default()
+                };
+
+                context.renderer.textures.replace(
+                    self.texture_id,
+                    imgui_wgpu::Texture::new(&context.device, &context.renderer, texture_config),
+                );
+            }
+        };
     }
 
-    fn convert_color(color: wgpu::Color) -> u8 {
+    fn convert_color(color : wgpu::Color) -> u8 {
+
         let r = color.r as u8;
 
         let g = color.g as u8;
@@ -141,10 +159,5 @@ impl LayerRenderer {
         color1
     }
 
-    pub fn per_pixel(
-        x: u32,
-        y: u32,
-    ) -> image::Rgba<u8> {
-        image::Rgba([155, 155, 155, 1])
-    }
+    pub fn per_pixel(x : u32, y : u32) -> image::Rgba<u8> { image::Rgba([155, 155, 155, 1]) }
 }
